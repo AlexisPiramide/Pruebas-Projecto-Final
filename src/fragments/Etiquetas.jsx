b66b45539528c093d5fcd4fca8f684c0f74f7b68
AlexisPiramide <220240@fppiramide.com>
2024-03-19 13:35:48 +0100

    Hacerlo todo en componentes mas pequeños

--- src/fragments/Etiquetas.jsx
+++ src/fragments/Etiquetas.jsx
@@ -1,6 +1,9 @@
 import "../style/Etiquetas.css"
+import { useState } from "react";
 
-export default function Etiquetas({ etiquetas, etiquetasSeleccionadas, setEtiquetasSeleccionadas, setOpcionesBusqueda}) {
+export default function Etiquetas({ etiquetas, etiquetasSeleccionadas, setEtiquetasSeleccionadas, setOpcionesBusqueda }) {
+
+    const [opcionTexto, setOpcionTexto] = useState('Seleciona una opcion de busqueda');
 
     // Esta funcion filtra las etiquetas seleccionadas y las agrega o quita a las etiquetas selecionadas 
     const selecionarEtiqueta = (e) => {
@@ -14,7 +17,7 @@ export default function Etiquetas({ etiquetas, etiquetasSeleccionadas, setEtique
         } else {
             setEtiquetasSeleccionadas([...etiquetasSeleccionadas, selectedEtiqueta]);
         }
-        
+
         animacionEtiquetaCss(e.target);
     }
 
@@ -29,21 +32,33 @@ export default function Etiquetas({ etiquetas, etiquetasSeleccionadas, setEtique
     const seleccionOpciones = (e) => {
         e.preventDefault();
         const opcion = e.target.id;
+        let texto = '';
+        (opcion == 'exclusiva') ? texto = 'Solo se mostraran ofertas con estas etiquetas' : texto = 'Se mostrarán todas las ofertas con alguna de estas etiquetas';
+        setOpcionTexto(texto)
         setOpcionesBusqueda(opcion);
     }
 
+    const esconderEtiquetas = () => {
+        
+    }
 
     return (
-        <div class="secion-etiquetas">
-            <input class="search-bar" type="text" placeholder="Buscar ofertas..." />
-            <div class="container-tags">
+        <div className="secion-etiquetas">
+            <div>
+                <input className="search-bar" type="text" placeholder="Buscar ofertas..." />
+                <button onClick={esconderEtiquetas}>Esconder Etiquetas</button>
+            </div>
+            <div className="container-tags">
                 {etiquetas.map((etiqueta) => (
-                    <button key={etiqueta.id} class="tag" type="checkbox" onClick={(e) => (selecionarEtiqueta(e))}>{etiqueta.nombre}<input type="checkbox"/></button>
+                    <button key={etiqueta.id} className="tag" type="checkbox" onClick={(e) => (selecionarEtiqueta(e))}>{etiqueta.nombre}<input type="checkbox" /></button>
                 ))}
             </div>
-            <div class="opciones-etiquetas">
-                <button id="exclusiva" onClick={(e)=> seleccionOpciones(e)}>Busqueda Exclusiva*<input type="radio" name="busqueda" value="exclusiva"/></button>
-                <button id="inclusiva"  onClick={(e)=> seleccionOpciones(e)}>Busqueda Inclusiva*<input type="radio" name="busqueda" value="inclusiva"/></button>
+            <div className="opciones-etiquetas">
+                <p id='texto-opcion'>{opcionTexto}</p>
+                <div>
+                    <button id="exclusiva" onClick={(e) => seleccionOpciones(e)}>Busqueda Exclusiva*<input type="radio" name="busqueda" value="exclusiva" /></button>
+                    <button id="inclusiva" onClick={(e) => seleccionOpciones(e)}>Busqueda Inclusiva*<input type="radio" name="busqueda" value="inclusiva" /></button>
+                </div>
             </div>
         </div>
     )
